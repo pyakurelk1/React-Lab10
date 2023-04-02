93733a61e0cc92e920db1410bd11432703dce401
pyakurelk1 <122557490+pyakurelk1@users.noreply.github.com>
2023-04-02 01:11:45 -0400

    Update App.js
    updated lab 11

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
-import React from 'react';
-import './styles/Container.css';
-import './styles/List.css';
-import './styles/Selector.css';
-import Container from './components/Container';
-import List from './components/List';
-import Selector from './components/Selector';
+import './App.css';
+import React, { useState } from 'react';
+import request from 'browser-request';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [selectedList, setSelectedList] = React.useState('List 1');
-  const lists = {
-    'List 1': ['Item 1', 'Item 2', 'Item 3'],
-    'List 2': ['Item A', 'Item B', 'Item C'],
-    'List 3': ['Thing 1', 'Thing 2', 'Thing 3']
+  const [cityName, setCityName] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+
+  const apiKey = 'eb819c14c08ff9e4cf99ba3381655186';
+
+  const fetchWeatherData = () => {
+    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+    request({ url: apiUrl, json: true }, (error, response, body) => {
+      if (error) {
+        console.error('Error fetching data:', error);
+      } else if (response.statusCode === 200) {
+        setWeatherData(body);
+      } else {
+        console.error('Unable to fetch data:', response.statusCode);
+      }
+    });
+  };
+
+  const renderWeatherData = () => {
+    if (weatherData) {
+      const weatherDescription = weatherData.weather[0].description;
+      const temperature = weatherData.main.temp;
+
+      return (
+        <div>
+          Weather in {cityName}: {weatherDescription}, {temperature}°C
+        </div>
+      );
+    }
+    return null;
   };
 
   return (
-    <Container>
-      <Selector
-        options={Object.keys(lists)}
-        selectedOption={selectedList}
-        onOptionSelect={(e) => setSelectedList(e.currentTarget.value)}
-      />
-      <List items={lists[selectedList]} />
-    </Container>
+      <div className="container mt-5">
+        <h1 className="text-center mb-4">Weather App</h1>
+        <div className="d-flex justify-content-center">
+          <input
+            type="text"
+            className="form-control w-50"
+            value={cityName}
+            onChange={(e) => setCityName(e.target.value)}
+            placeholder="Enter city name"
+          />
+          <button className="btn btn-primary ms-3" onClick={fetchWeatherData}>
+            Get Weather
+          </button>
+        </div>
+        <div className="text-center mt-4">{renderWeatherData()}</div>
+      </div>
   );
 }
 
